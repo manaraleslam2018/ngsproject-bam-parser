a734f61d2c9e18cec5916195590859fa9cd89d04
manaraleslam2018 <48625757+manaraleslam2018@users.noreply.github.com>
2019-04-21 13:05:56 +0200

    Update plant_code.sh

--- plant_code.sh
+++ plant_code.sh
@@ -1,42 +1,41 @@
 https://en.wikipedia.org/wiki/List_of_sequenced_plant_genomes#Press_releases_announcing_sequencing
-this link was useful in selecting an organism.
+this link was useful in selecting an organism with low quality genome (Corchorus capsularis CVL-1).
 
-https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=SRR1790382
-SRR1790382
-
-download samples
+1- download samples from SRA
 '''
+mkdir /home/manar/ngs1_project/plant_samples && cd /home/manar/ngs1_project/plant_samples
 wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR179/SRR1790382/SRR1790382.sra
 '''
-to dwonload gtf 
-'''
-wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/gff3/corchorus_capsularis/Corchorus_capsularis.CCACVL1_1.0.43.gff3.gz
-'''
-download fasta file refrence (ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/corchorus_capsularis/dna/)
-'''
-ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/corchorus_capsularis/dna/Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal.fa.gz
-gunzip Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal.fa.gz
-'''
-
-to choose the fasta file i can use for alignment 
-
-http://genomespot.blogspot.com/2015/06/mapping-ngs-data-which-genome-version.html
-https://bioinformatics.stackexchange.com/questions/540/what-ensembl-genome-version-should-i-use-for-alignments-e-g-toplevel-fa-vs-p
-
-Repeat Masking?
-The short answer is that for the purpose of mapping NGS reads, you don't want to use a RepeatMasked genome. Repeat-Masking is a process to identify all repetitive parts of the genome such as LINE-1 and Alu and replace these sequences with "N". While this may sound like a good idea, it's not because mapping software are specifically designed to handle reads from these repeats and have sophisticated heuristics to either discard those alignments or find all possible alignments. So select a build that has no masking and avoid any with "rm" in the name (ie: Homo_sapiens.GRCh38.dna_rm.primary_assembly.fa.gz). The files with "sm" in the name are "soft masked" which means instead of the repeat regions being given Ns, they are converted to lower-case. These are OK to use because most major aligners recognise lower-case letters as valid bases.
-
-extract fastq from sra data 
+2- extract fastq from sra data 
 '''
 fastq-dump --outdir . --gzip --split-3 SRR1790382.sra
 '''
-fastqc 
+3- dwonload gtf reference file 
+'''
+mkdir /home/manar/ngs1_project/plant_refernce && cd /home/manar/ngs1_project/plant_refernce
+wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/gff3/corchorus_capsularis/Corchorus_capsularis.CCACVL1_1.0.43.gff3.gz
+'''
+4- download fasta file refrence (ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/corchorus_capsularis/dna/)
+'''
+wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/corchorus_capsularis/dna/Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal.fa.gz
+gunzip Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal.fa.gz
+'''
+
+I was confused between different fasta files to use as a refernec. To choose the fasta file which I can use for alignment,
+I searched and found that I should avoid  any file with "rm" in the name but I can use the files with "sm" in the name are "soft masked" or without masking. 
+the answer available in these websites:
+http://genomespot.blogspot.com/2015/06/mapping-ngs-data-which-genome-version.html
+https://bioinformatics.stackexchange.com/questions/540/what-ensembl-genome-version-should-i-use-for-alignments-e-g-toplevel-fa-vs-p
+
+5- fastqc 
+'''
+mkdir /home/manar/ngs1_project/plant_samples/fastqc/ && cd /home/manar/ngs1_project/plant_samples/fastqc 
+cp /home/manar/ngs1_project/plant_samples/SRR1790382_1.fastq.gz .
+cp /home/manar/ngs1_project/plant_samples/SRR1790382_2.fastq.gz .
 for f in /home/manar/ngs1_project/plant_samples/fastqc/*.fastq.gz;do fastqc -t 1 -f fastq -noextract $f;done
+'''
 
-/home/manar/ngs1_project/plant_samples/fastqc/SRR1790382_1.fastq.gz
-
-
-4- Trimming
+6- Trimming
 '''
 mkdir ~/ngs1_project/plant_trimmed && cd ~/ngs1_project/plant_trimmed
  
@@ -50,9 +49,8 @@ adap="/home/manar/anaconda3/envs/ngs1/share/trimmomatic-0.39-0/adapters";
 trimmomatic PE -threads 1 -phred33 -trimlog trimLogFile -summary statsSummaryFile  $f1 $f2 $newf1 $newf1U $newf2 $newf2U ILLUMINACLIP:$adap/TruSeq3-PE.fa:2:30:10:1 SLIDINGWINDOW:4:15 MINLEN:36
 '''
 
-HISAT allignment 
+7- HISAT allignment 
 index your genome
-
 '''
 mkdir /home/manar/ngs1_project/plant_hisat_alignment/hisatIndex && cd /home/manar/ngs1_project/plant_hisat_alignment/hisatIndex
 
@@ -60,25 +58,21 @@ ln -s /home/manar/ngs1_project/plant_refernce/Corchorus_capsularis.CCACVL1_1.0.d
 ln -s /home/manar/ngs1_project/plant_refernce/Corchorus_capsularis.CCACVL1_1.0.43.gff3 .
 hisat2_extract_splice_sites.py /home/manar/ngs1_project/plant_refernce/Corchorus_capsularis.CCACVL1_1.0.43.gff3 > splicesites.tsv
 hisat2_extract_exons.py /home/manar/ngs1_project/plant_refernce/Corchorus_capsularis.CCACVL1_1.0.43.gff3 > exons.tsv
-
-
 hisat2-build -p 1 --ss splicesites.tsv --exon exons.tsv /home/manar/ngs1_project/plant_refernce/Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal.fa Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal_indexed
 '''
 
 sequence alignment
 '''
 cd ~/ngs1_project/plant_hisat_alignment
-
-
 R1="/home/manar/ngs1_project/plant_trimmed/plant_sample_r1.pe.trim.fastq.gz"
 R2="/home/manar/ngs1_project/plant_trimmed/plant_sample_r2.pe.trim.fastq.gz"
 hisat2 -p 1 -x hisatIndex/Corchorus_capsularis.CCACVL1_1.0.dna.nonchromosomal_indexed --rna-strandness RF -1 $R1 -2 $R2 -S plant_hisat_aligment.sam
-
 '''
+
 stats 
 
 '''
-    samtools flagstat /home/manar/ngs1_project/plant_hisat_alignment/plant_hisat_aligment.sam > plant_haisat_alignment_stats.out
+samtools flagstat /home/manar/ngs1_project/plant_hisat_alignment/plant_hisat_aligment.sam > plant_haisat_alignment_stats.out
 
 '''
 
@@ -88,8 +82,11 @@ samtools view -bS /home/manar/ngs1_project/plant_hisat_alignment/plant_hisat_ali
 samtools sort /home/manar/ngs1_project/plant_hisat_alignment/plant_hisat_aligment.bam -o /home/manar/ngs1_project/plant_hisat_alignment/plant_hisat_aligment.bam_sorted.bam
 
 '''
- Proper discordant read extraction - samblaster vs. samtools #193 
+Bam parsers include ( pysam or pysbm or samtools) I used samtools 
+
+and Proper discordant read extraction samtools
 https://github.com/arq5x/lumpy-sv/issues/193
+
 '''
 samtools view -b -F 1294 plant_hisat_aligment.bam_sorted.bam > plant1294_sample.discordants_sorted.bam
 
